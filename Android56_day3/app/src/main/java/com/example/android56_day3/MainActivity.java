07c5ae0146291047de4cdcf4a0eda31de8c93d86
tomhuyquang@gmail.com <tomhuyquang@gmail.com>
2025-05-16 17:06:58 +0700

    android day 3

--- Android56_day3/app/src/main/java/com/example/android56_day3/MainActivity.java
+++ Android56_day3/app/src/main/java/com/example/android56_day3/MainActivity.java
@@ -14,7 +14,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.layout_relative_demo);
+        setContentView(R.layout.activity_main);
 
     }
 }
